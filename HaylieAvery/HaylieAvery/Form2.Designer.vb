ed46d0611fff96d957c15b175944ffeb6285accd
Haylie <hatsc136@learner.dvusd.org>
2024-03-21 12:42:44 -0700

    Form 2

---
+++ HaylieAvery/HaylieAvery/Form2.Designer.vb
@@ -0,0 +1,346 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form2
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TotalLabel = New System.Windows.Forms.Label()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.PictureBox15 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox14 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox12 = New System.Windows.Forms.PictureBox()
+        Me.PictureBoxWall1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox10 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox9 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox8 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox7 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox6 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox5 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBoxWall2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBoxWall4 = New System.Windows.Forms.PictureBox()
+        Me.PictureBoxWall3 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox15, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox14, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox12, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBoxWall1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox10, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox9, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox8, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox7, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBoxWall2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBoxWall4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBoxWall3, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'TotalLabel
+        '
+        Me.TotalLabel.AutoSize = True
+        Me.TotalLabel.Location = New System.Drawing.Point(1277, 117)
+        Me.TotalLabel.Name = "TotalLabel"
+        Me.TotalLabel.Size = New System.Drawing.Size(0, 16)
+        Me.TotalLabel.TabIndex = 61
+        '
+        'Button4
+        '
+        Me.Button4.Font = New System.Drawing.Font("Dutch801 XBd BT", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button4.Location = New System.Drawing.Point(1234, 415)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(99, 31)
+        Me.Button4.TabIndex = 60
+        Me.Button4.Text = "Windows"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Font = New System.Drawing.Font("Dutch801 XBd BT", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(872, 415)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 31)
+        Me.Button3.TabIndex = 59
+        Me.Button3.Text = "Doors"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Dutch801 XBd BT", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(536, 415)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 31)
+        Me.Button2.TabIndex = 58
+        Me.Button2.Text = "Floors"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Dutch801 XBd BT", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(179, 415)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 31)
+        Me.Button1.TabIndex = 57
+        Me.Button1.Text = "Walls"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'PictureBox15
+        '
+        Me.PictureBox15.Image = Global.HaylieAvery.My.Resources.Resources.window5
+        Me.PictureBox15.Location = New System.Drawing.Point(1274, 230)
+        Me.PictureBox15.Name = "PictureBox15"
+        Me.PictureBox15.Size = New System.Drawing.Size(175, 118)
+        Me.PictureBox15.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox15.TabIndex = 77
+        Me.PictureBox15.TabStop = False
+        '
+        'PictureBox14
+        '
+        Me.PictureBox14.Image = Global.HaylieAvery.My.Resources.Resources.window2
+        Me.PictureBox14.Location = New System.Drawing.Point(1302, 86)
+        Me.PictureBox14.Name = "PictureBox14"
+        Me.PictureBox14.Size = New System.Drawing.Size(141, 101)
+        Me.PictureBox14.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox14.TabIndex = 76
+        Me.PictureBox14.TabStop = False
+        '
+        'PictureBox12
+        '
+        Me.PictureBox12.Image = Global.HaylieAvery.My.Resources.Resources.window1
+        Me.PictureBox12.Location = New System.Drawing.Point(1131, 65)
+        Me.PictureBox12.Name = "PictureBox12"
+        Me.PictureBox12.Size = New System.Drawing.Size(140, 111)
+        Me.PictureBox12.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox12.TabIndex = 75
+        Me.PictureBox12.TabStop = False
+        '
+        'PictureBoxWall1
+        '
+        Me.PictureBoxWall1.Image = Global.HaylieAvery.My.Resources.Resources.wall4
+        Me.PictureBoxWall1.Location = New System.Drawing.Point(95, 125)
+        Me.PictureBoxWall1.Name = "PictureBoxWall1"
+        Me.PictureBoxWall1.Size = New System.Drawing.Size(113, 106)
+        Me.PictureBoxWall1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBoxWall1.TabIndex = 74
+        Me.PictureBoxWall1.TabStop = False
+        Me.PictureBoxWall1.Visible = False
+        '
+        'PictureBox10
+        '
+        Me.PictureBox10.Image = Global.HaylieAvery.My.Resources.Resources.floor1
+        Me.PictureBox10.Location = New System.Drawing.Point(468, 259)
+        Me.PictureBox10.Name = "PictureBox10"
+        Me.PictureBox10.Size = New System.Drawing.Size(110, 106)
+        Me.PictureBox10.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox10.TabIndex = 73
+        Me.PictureBox10.TabStop = False
+        '
+        'PictureBox9
+        '
+        Me.PictureBox9.Image = Global.HaylieAvery.My.Resources.Resources.Floor2
+        Me.PictureBox9.Location = New System.Drawing.Point(596, 259)
+        Me.PictureBox9.Name = "PictureBox9"
+        Me.PictureBox9.Size = New System.Drawing.Size(107, 106)
+        Me.PictureBox9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox9.TabIndex = 72
+        Me.PictureBox9.TabStop = False
+        '
+        'PictureBox8
+        '
+        Me.PictureBox8.Image = Global.HaylieAvery.My.Resources.Resources.floor3
+        Me.PictureBox8.Location = New System.Drawing.Point(596, 125)
+        Me.PictureBox8.Name = "PictureBox8"
+        Me.PictureBox8.Size = New System.Drawing.Size(107, 114)
+        Me.PictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox8.TabIndex = 71
+        Me.PictureBox8.TabStop = False
+        '
+        'PictureBox7
+        '
+        Me.PictureBox7.Image = Global.HaylieAvery.My.Resources.Resources.Floor4
+        Me.PictureBox7.Location = New System.Drawing.Point(453, 145)
+        Me.PictureBox7.Name = "PictureBox7"
+        Me.PictureBox7.Size = New System.Drawing.Size(125, 86)
+        Me.PictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox7.TabIndex = 70
+        Me.PictureBox7.TabStop = False
+        '
+        'PictureBox6
+        '
+        Me.PictureBox6.Image = Global.HaylieAvery.My.Resources.Resources.Door3
+        Me.PictureBox6.Location = New System.Drawing.Point(913, 22)
+        Me.PictureBox6.Name = "PictureBox6"
+        Me.PictureBox6.Size = New System.Drawing.Size(67, 165)
+        Me.PictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox6.TabIndex = 69
+        Me.PictureBox6.TabStop = False
+        '
+        'PictureBox5
+        '
+        Me.PictureBox5.Image = Global.HaylieAvery.My.Resources.Resources.Door2
+        Me.PictureBox5.Location = New System.Drawing.Point(796, 259)
+        Me.PictureBox5.Name = "PictureBox5"
+        Me.PictureBox5.Size = New System.Drawing.Size(64, 149)
+        Me.PictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox5.TabIndex = 68
+        Me.PictureBox5.TabStop = False
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.Image = Global.HaylieAvery.My.Resources.Resources.Door4
+        Me.PictureBox4.Location = New System.Drawing.Point(900, 193)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(80, 178)
+        Me.PictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox4.TabIndex = 67
+        Me.PictureBox4.TabStop = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.Image = Global.HaylieAvery.My.Resources.Resources.window4
+        Me.PictureBox3.Location = New System.Drawing.Point(1131, 205)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(100, 143)
+        Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox3.TabIndex = 66
+        Me.PictureBox3.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = Global.HaylieAvery.My.Resources.Resources.Door1
+        Me.PictureBox2.Location = New System.Drawing.Point(796, 65)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(82, 174)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox2.TabIndex = 65
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBoxWall2
+        '
+        Me.PictureBoxWall2.Image = Global.HaylieAvery.My.Resources.Resources.wall3
+        Me.PictureBoxWall2.Location = New System.Drawing.Point(269, 116)
+        Me.PictureBoxWall2.Name = "PictureBoxWall2"
+        Me.PictureBoxWall2.Size = New System.Drawing.Size(93, 128)
+        Me.PictureBoxWall2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBoxWall2.TabIndex = 64
+        Me.PictureBoxWall2.TabStop = False
+        Me.PictureBoxWall2.Visible = False
+        '
+        'PictureBoxWall4
+        '
+        Me.PictureBoxWall4.Image = Global.HaylieAvery.My.Resources.Resources.wall2
+        Me.PictureBoxWall4.Location = New System.Drawing.Point(226, 265)
+        Me.PictureBoxWall4.Name = "PictureBoxWall4"
+        Me.PictureBoxWall4.Size = New System.Drawing.Size(159, 83)
+        Me.PictureBoxWall4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBoxWall4.TabIndex = 63
+        Me.PictureBoxWall4.TabStop = False
+        Me.PictureBoxWall4.Visible = False
+        '
+        'PictureBoxWall3
+        '
+        Me.PictureBoxWall3.Image = Global.HaylieAvery.My.Resources.Resources.wall1
+        Me.PictureBoxWall3.Location = New System.Drawing.Point(88, 259)
+        Me.PictureBoxWall3.Name = "PictureBoxWall3"
+        Me.PictureBoxWall3.Size = New System.Drawing.Size(120, 106)
+        Me.PictureBoxWall3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBoxWall3.TabIndex = 62
+        Me.PictureBoxWall3.TabStop = False
+        Me.PictureBoxWall3.Visible = False
+        '
+        'Form2
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1549, 539)
+        Me.Controls.Add(Me.PictureBox15)
+        Me.Controls.Add(Me.PictureBox14)
+        Me.Controls.Add(Me.PictureBox12)
+        Me.Controls.Add(Me.PictureBoxWall1)
+        Me.Controls.Add(Me.PictureBox10)
+        Me.Controls.Add(Me.PictureBox9)
+        Me.Controls.Add(Me.PictureBox8)
+        Me.Controls.Add(Me.PictureBox7)
+        Me.Controls.Add(Me.PictureBox6)
+        Me.Controls.Add(Me.PictureBox5)
+        Me.Controls.Add(Me.PictureBox4)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBoxWall2)
+        Me.Controls.Add(Me.PictureBoxWall4)
+        Me.Controls.Add(Me.PictureBoxWall3)
+        Me.Controls.Add(Me.TotalLabel)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form2"
+        Me.Text = "Form2"
+        CType(Me.PictureBox15, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox14, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox12, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBoxWall1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox10, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox9, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox8, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox7, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBoxWall2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBoxWall4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBoxWall3, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PictureBox15 As PictureBox
+    Friend WithEvents PictureBox14 As PictureBox
+    Friend WithEvents PictureBox12 As PictureBox
+    Friend WithEvents PictureBoxWall1 As PictureBox
+    Friend WithEvents PictureBox10 As PictureBox
+    Friend WithEvents PictureBox9 As PictureBox
+    Friend WithEvents PictureBox8 As PictureBox
+    Friend WithEvents PictureBox7 As PictureBox
+    Friend WithEvents PictureBox6 As PictureBox
+    Friend WithEvents PictureBox5 As PictureBox
+    Friend WithEvents PictureBox4 As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents PictureBoxWall2 As PictureBox
+    Friend WithEvents PictureBoxWall4 As PictureBox
+    Friend WithEvents PictureBoxWall3 As PictureBox
+    Friend WithEvents TotalLabel As Label
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
+End Class
