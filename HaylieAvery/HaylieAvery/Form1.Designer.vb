d01435499b36a8f05a1d3310dccd50fc2919ea08
Haylie <hatsc136@learner.dvusd.org>
2024-03-21 13:11:39 -0700

    Form 1 to form 2

--- HaylieAvery/HaylieAvery/Form1.Designer.vb
+++ HaylieAvery/HaylieAvery/Form1.Designer.vb
@@ -22,18 +22,48 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+<<<<<<< Updated upstream
         Me.TotalLabel = New System.Windows.Forms.Label()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.Label1 = New System.Windows.Forms.Label()
+=======
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+>>>>>>> Stashed changes
         Me.SuspendLayout()
         '
-        'TotalLabel
+        'Label1
         '
-        Me.TotalLabel.AutoSize = True
-        Me.TotalLabel.Location = New System.Drawing.Point(1195, 100)
-        Me.TotalLabel.Name = "TotalLabel"
-        Me.TotalLabel.Size = New System.Drawing.Size(0, 16)
-        Me.TotalLabel.TabIndex = 4
+        Me.Label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Label1.Font = New System.Drawing.Font("MS PGothic", 25.8!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(451, 174)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(460, 49)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "revamp construction company"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(30, 77)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(369, 231)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 1
+        Me.PictureBox1.TabStop = False
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(1137, 447)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(268, 47)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Click to see products!"
+        Me.Button1.UseVisualStyleBackColor = True
         '
         'Button1
         '
@@ -58,17 +88,32 @@ Partial Class Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+<<<<<<< Updated upstream
         Me.ClientSize = New System.Drawing.Size(651, 396)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.TotalLabel)
+=======
+        Me.ClientSize = New System.Drawing.Size(1504, 525)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.ForeColor = System.Drawing.Color.LightCoral
+>>>>>>> Stashed changes
         Me.Name = "Form1"
         Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
-        Me.PerformLayout()
 
     End Sub
+<<<<<<< Updated upstream
     Friend WithEvents TotalLabel As Label
     Friend WithEvents Button1 As Button
     Friend WithEvents Label1 As Label
+=======
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Button1 As Button
+>>>>>>> Stashed changes
 End Class
--- HaylieAvery/HaylieAvery/Form1.vb
+++ HaylieAvery/HaylieAvery/Form1.vb
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 ﻿Imports System.Reflection.Emit
 
 Public Class Form1
@@ -5,6 +6,17 @@ Public Class Form1
         Me.Hide()
         Dim f2 As New Form2
         f2.ShowDialog()
+=======
+﻿
+
+Public Class Form1
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.Hide()
+        Dim f As New Form2
+        f.ShowDialog()
+
+>>>>>>> Stashed changes
         Me.Show()
     End Sub
 End Class
